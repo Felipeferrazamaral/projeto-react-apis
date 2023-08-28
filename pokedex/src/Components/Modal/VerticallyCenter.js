1514238d24b147def25359e39e8cbc9314e7cb13
Felipeferrazamaral <felipeferrazamaral@gmail.com>
2023-08-28 16:45:57 -0300

    projeto-finalizado

---
+++ pokedex/src/Components/Modal/VerticallyCenter.js
@@ -0,0 +1,34 @@
+import React, { useContext } from 'react';
+import GlobalContext from '../../contexts/GlobalContext';
+import {
+    Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalCloseButton,
+    Image,
+} from '@chakra-ui/react';
+import images from '../../assets/importImages';
+
+const VerticallyCenter = () => {
+    const context = useContext(GlobalContext);
+    const { chooseImage, isOpen, onClose } = context;
+    return (
+        <>
+            <Modal onClose={onClose} isOpen={isOpen} isCentered>
+                <ModalOverlay />
+                <ModalContent>
+                    <ModalCloseButton />
+                    <Image
+                        src={
+                            chooseImage === 'capture'
+                                ? images.capture
+                                : images.remove
+                        }
+                    />
+                </ModalContent>
+            </Modal>
+        </>
+    );
+};
+
+export default VerticallyCenter;
